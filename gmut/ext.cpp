377e162209e8e049b0c5e9e516e05dd132ab7592
Tobias Schlememr <keinstein@users.berlios.de>
2005-06-16 13:55:28 +0000

    Doxygen Doku

--- gmut/ext.cpp
+++ gmut/ext.cpp
@@ -1,11 +1,21 @@
-// ##################################################################
-// EXT (some tine fancy tools for file names)
-// ##################################################################
+/** \file ext.cpp
+ ******************************************************************
+ * Some tine fancy tools for file names
+ ******************************************************************/
+
 
 #include "ext.h"
 #include <string.h>
 
-char sHelp[120];
+char sHelp[120]; //!< return value for ExtCheck and ExtChange functions
+
+/**
+ **************************************************************************
+ *  Append a extension to a file name if it does not exist.
+ *  \param[in] fileName Filename the extension has to be added to
+ *  \param[in] extension Extension which should be added 
+ *  \bug possible buffer overflow for sHelp (no length check)
+ **************************************************************************/
 
 char *ExtCheck(char *fileName, char *extension)
 {
@@ -18,6 +28,16 @@ char *ExtCheck(char *fileName, char *extension)
   return sHelp;
 }
 
+/**
+ *************************************************************************
+ *  Exchanges the extension of a file name.
+ * 
+ * \param[in] fileName Filename the extension has to be added to
+ * \param[in] extension Extension which should be added 
+ * \bug possible buffer overflow for sHelp (no length check)
+ * \bug first dot in filename used to detect extension, but not last one.
+ *************************************************************************/
+  
 char *ExtChange(char *fileName, char *extension)
 {
   strcpy(sHelp, fileName);
