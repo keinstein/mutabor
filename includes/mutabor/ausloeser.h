52c07f53614fd7aed925f5b3aa700c7d6750eaa6
Tobias Schlemmer <keinstein@users.berlios.de>
2005-07-08 14:50:14 +0000

    Neu: 	print_ausloeser 	check_ausloeser 	get_last_ausloeser

--- includes/mutabor/ausloeser.h
+++ includes/mutabor/ausloeser.h
@@ -49,13 +49,18 @@ namespace mutabor {
       } u; /**< Daten des Auslösers */
     };
     
+    void print_ausloeser (struct ausloeser * this_);
+
     void init_ausloeser (void);
     void get_ausloeser_default (void);
     void get_ausloeser_harmonie (void);
     void get_ausloeser_harmonie_form (void);
     void get_ausloeser_taste (char * name);
     void get_ausloeser_midi_in (void);
-    
+    void check_ausloeser (struct ausloeser * ausloeser, char * name);
+
+    struct ausloeser * get_last_ausloeser (void);
+
 #ifdef __cplusplus 
   } // extern "C"
 } // namespace mutabor
