2472f16db121dc7c7b6573048cfff89dcaea48e9
Tobias Schlemmer <keinstein@users.berlios.de>
2005-11-03 14:31:44 +0000

    new file header includes corrected interpreter data structures

--- includes/mutabor/harmonie.h
+++ includes/mutabor/harmonie.h
@@ -2,15 +2,24 @@
  ********************************************************************
  * Harmonien.
  *
+ * $Id: harmonie.h,v 1.3 2005/11/03 14:31:44 keinstein Exp $
  * \author Tobias Schlemmer <keinstein_junior@gmx.net>
- * \date 2005
- * \version 0.1
+ * \date $Date: 2005/11/03 14:31:44 $
+ * \version $Revision: 1.3 $
+ *
+ * $Log: harmonie.h,v $
+ * Revision 1.3  2005/11/03 14:31:44  keinstein
+ * new file header
+ * includes corrected
+ * interpreter data structures
+ *
  ********************************************************************/
 
 #ifndef __HARMONIE_H_INCLUDED
 #define __HARMONIE_H_INCLUDED
 
 #include "mutabor/interval.h"
+#include "mutabor/bad_decl.h"
 
 #ifdef __cplusplus
 namespace mutabor {
@@ -27,6 +36,11 @@ namespace mutabor {
       struct harmonie * next;        /**< Nächste Harmonie */
     };
 
+    /** Pattern-Struktur für Analyse der Harmonieformen. */
+    typedef struct PTRN {
+      int tonigkeit[MAX_BREITE]; /**< Tonigkeiten */
+    } PATTERNN;
+
     void drucke_harmonie (struct harmonie * this_);
 
     void get_new_harmonie (char * name, int bezugstaste);
@@ -35,6 +49,7 @@ namespace mutabor {
 				 int nach_taste);
 
     struct harmonie * get_harmonie (char * name, struct harmonie * liste);
+    PATTERNN * expand_pattern (char * harmonie_name);
 
     extern struct harmonie       *  list_of_harmonien;
     int        tmp_vor_taste; /**< Zwischenspeicher für tiefste Taste einer Harmonie */
