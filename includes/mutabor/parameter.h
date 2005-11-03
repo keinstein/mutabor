91613b5ed316af3ee9819e1223a897974f3d8123
Tobias Schlemmer <keinstein@users.berlios.de>
2005-11-03 14:35:54 +0000

    new file header includes corrected interpreter data structures and functions

--- includes/mutabor/parameter.h
+++ includes/mutabor/parameter.h
@@ -2,24 +2,40 @@
  ********************************************************************
  * Alles zu Parametern.
  *
+ * $Id: parameter.h,v 1.3 2005/11/03 14:35:54 keinstein Exp $
  * \author Tobias Schlemmer <keinstein_junior@gmx.net>
- * \date 2005
- * \version 0.1
+ * \date $Date: 2005/11/03 14:35:54 $
+ * \version $Revision: 1.3 $
+ * 
+ * $Log: parameter.h,v $
+ * Revision 1.3  2005/11/03 14:35:54  keinstein
+ * new file header
+ * includes corrected
+ * interpreter data structures and functions
+ *
  ********************************************************************/
 
+struct interpreter_parameter_liste;
 #ifndef __PARAMETER_H_INCLUDED
 #define __PARAMETER_H_INCLUDED
 
+#include "mutabor/argument.h"
+#include "mutabor/bad_decl.h"
 
 #ifdef __cplusplus
 namespace mutabor {
   extern "C" {
 #endif
-
     /** Einfach verkettete Liste mit Parameternamen */
     struct parameter_liste {
       char * name;                    /**< Name des Parameters */
       struct parameter_liste * next;  /**< Nächster Parameter */
+    };
+
+    /** Einfach verkettete Liste mit Parametern */
+    struct interpreter_parameter_liste {
+      int (* werte_feld) [MAX_BOX]; /**< Werte für jede Box */
+      struct interpreter_parameter_liste * next; /**< Nächster Parameter */
     };
 
     /** Ganzzahliger Parametertyp */
@@ -34,7 +50,10 @@ namespace mutabor {
     int parameter_nummer (int aktueller_index, char * such_name, 
 			  struct parameter_liste * knoten);
     struct parameter_liste * get_last_parameter_liste (void);
-
+    struct interpreter_parameter_liste * gen_parameter_liste (
+			struct argument_liste * liste,
+			struct interpreter_parameter_liste * bezugs_liste);
+    extern struct interpreter_parameter_liste * start_parameter_liste;
 #ifdef __cplusplus 
   } // extern "C"
 } // namespace mutabor
