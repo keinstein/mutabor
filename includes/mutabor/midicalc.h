d473bcc2e8652e23efaf3fa627a354e08506ceb0
Tobias Schlemmer <keinstein@users.berlios.de>
2005-11-03 14:33:36 +0000

    new header includes corrected interpreter data structures and functions

--- includes/mutabor/midicalc.h
+++ includes/mutabor/midicalc.h
@@ -2,13 +2,30 @@
  ********************************************************************
  * MIDI-Berechnungen.
  *
+ * $Id: midicalc.h,v 1.3 2005/11/03 14:33:36 keinstein Exp $
  * \author Tobias Schlemmer <keinstein_junior@gmx.net>
- * \date 2005
- * \version 0.1
+ * \date $Date: 2005/11/03 14:33:36 $
+ * \version $Revision: 1.3 $
+ *
+ * $Log: midicalc.h,v $
+ * Revision 1.3  2005/11/03 14:33:36  keinstein
+ * new header
+ * includes corrected
+ * interpreter data structures and functions
+ *
  ********************************************************************/
 
 #ifndef __MIDICALC_H_INCLUDED
 #define __MIDICALC_H_INCLUDED
+
+#ifdef HAVE_MATH_H
+# include<math.h>
+#endif
+
+/* Zur Umrechnung in die Midi-Darstellung
+   als Gleitpunktzahl in long.
+   Erstes Byte Vorkommastellen, drei Bytes Nachkommastellen. */
+#define DOUBLE_TO_LONG(x) ((long int) ((x) * (1L<<24)))
 
 /** Frequenz nach Midi umrechnen. 
  * Es gilt: \f[0.05776226504666210908=\ln\left(\sqrt[12]2\right)\f].
@@ -39,7 +56,8 @@ namespace mutabor {
     void get_new_integer_in_integersequenz (int wert);
     int midi_list_laenge (struct midiliste *list);
     struct midiliste * get_last_integersequenz (void);
-
+    int * create_midi_scan_liste (struct midiliste * lauf);
+    
     void print_integersequenz (struct midiliste * this_);
 
 #ifdef __cplusplus 
