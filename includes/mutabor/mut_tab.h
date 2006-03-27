39c931b9343b56e2654f8c6471149573470e7d44
Tobias Schlemmer <keinstein@users.berlios.de>
2006-03-27 16:55:04 +0000

    headers reorganized, so that mutlex.h is no longer needed by muttab.{y,c}

--- includes/mutabor/mut_tab.h
+++ includes/mutabor/mut_tab.h
@@ -2,11 +2,14 @@
  ********************************************************************
  * Kopfdatei zu mut.c, welche aus mut.y erstellt wird.
  *
- * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/includes/mutabor/mut_tab.h,v 1.4 2005/11/03 14:34:30 keinstein Exp $
+ * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/includes/mutabor/mut_tab.h,v 1.5 2006/03/27 16:55:04 keinstein Exp $
  * \author Tobias Schlemmer <keinstein_junior@gmx.net>
- * \date $Date: 2005/11/03 14:34:30 $
- * \version $Revision: 1.4 $
+ * \date $Date: 2006/03/27 16:55:04 $
+ * \version $Revision: 1.5 $
  * $Log: mut_tab.h,v $
+ * Revision 1.5  2006/03/27 16:55:04  keinstein
+ * headers reorganized, so that mutlex.h is no longer needed by muttab.{y,c}
+ *
  * Revision 1.4  2005/11/03 14:34:30  keinstein
  * new parser calling convention
  *
@@ -35,9 +38,18 @@ void yyerror(char *);
 int yyparse ();
 */
 
+#include "mut.h"
+
+/* for mutlex.h */
+#ifndef YY_TYPEDEF_YY_SCANNER_T
+#define YY_TYPEDEF_YY_SCANNER_T
+typedef void* yyscan_t;
+#endif
+
 long anzahl_eingelesene_zeichen;
 
-int mutabor_parser_parse(void * _self);
+extern int mutabor_parser_parse(void * _self);
+extern int mutabor_parser_lex (YYSTYPE * yylval_param,YYLTYPE * yylloc_param ,yyscan_t yyscanner);
 /*
 int mutabor_parser_lex (YYSTYPE * yylval_param, YYLTYPE * yylloc_param, void * _self );
 void mutabor_parser_error(YYLTYPE * locp, void * _self, char const *s);
