ad0d0c0a1615a48c7d671845fb5df1a40b3ad216
Tobias Schlemmer <keinstein@users.berlios.de>
2006-03-27 16:53:05 +0000

    insert_in_lokale_liste moved from ausloeser to anweisung

--- includes/mutabor/anweisung.h
+++ includes/mutabor/anweisung.h
@@ -2,12 +2,15 @@
  ********************************************************************
  * Alles zu Anweisungen.
  *
- * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/includes/mutabor/anweisung.h,v 1.2 2005/07/08 14:48:35 keinstein Exp $
+ * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/includes/mutabor/anweisung.h,v 1.3 2006/03/27 16:53:05 keinstein Exp $
  * \author Tobias Schlemmer <keinstein_junior@gmx.net>
- * \date $Date: 2005/07/08 14:48:35 $
- * \version $Revision: 1.2 $
+ * \date $Date: 2006/03/27 16:53:05 $
+ * \version $Revision: 1.3 $
  * \log 
  * $Log: anweisung.h,v $
+ * Revision 1.3  2006/03/27 16:53:05  keinstein
+ * insert_in_lokale_liste moved from ausloeser to anweisung
+ *
  * Revision 1.2  2005/07/08 14:48:35  keinstein
  * Neu:
  * 	Kopf (CVS)
@@ -39,6 +42,8 @@ namespace mutabor {
     struct anweisung * get_last_anweisungs_liste (void);
     void init_anweisungs_liste (void);
     void get_new_anweisung (void);
+    void insert_in_lokale_liste (int instrument, struct anweisung * lauf,
+				 char * name_der_logik);
 
 
 #ifdef __cplusplus 
