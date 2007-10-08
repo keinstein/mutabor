fdc3b1356031840e36c5cc9088eab5d741f62ba9
Tobias Schlememr <keinstein@users.berlios.de>
2007-10-08 12:15:51 +0000

    Move FileNew and FileOpen from MutFrame to MutApp Enable multiple main windows MutApp: 	CmFileNew(), 	CmFileOpen() 	CmQuit(), 	RegisterFrame(), 	UnregisterFrame(): New functions. 	Rename OnAbout -> CmAbout
    	frames, quitting: New Member variables
    
    New Class: FrameHash;

--- muwx/MutApp.h
+++ muwx/MutApp.h
@@ -2,12 +2,27 @@
  ***********************************************************************
  * Mutabor Application.
  *
- * $Id: MutApp.h,v 1.5 2007/10/05 12:39:38 keinstein Exp $
+ * $Id: MutApp.h,v 1.6 2007/10/08 12:15:51 keinstein Exp $
  * \author R. Krauße <krausze@users.berlios.de>
- * \date $Date: 2007/10/05 12:39:38 $
- * \version $Revision: 1.5 $
+ * \date $Date: 2007/10/08 12:15:51 $
+ * \version $Revision: 1.6 $
  *
  * $Log: MutApp.h,v $
+ * Revision 1.6  2007/10/08 12:15:51  keinstein
+ * Move FileNew and FileOpen from MutFrame to MutApp
+ * Enable multiple main windows
+ * MutApp:
+ * 	CmFileNew(),
+ * 	CmFileOpen()
+ * 	CmQuit(),
+ * 	RegisterFrame(),
+ * 	UnregisterFrame(): New functions.
+ * 	Rename OnAbout -> CmAbout
+ *
+ * 	frames, quitting: New Member variables
+ *
+ * New Class: FrameHash;
+ *
  * Revision 1.5  2007/10/05 12:39:38  keinstein
  * first steps towards a real mac version:
  *  - automagic locale selection
@@ -39,16 +54,31 @@
 
 #include "MutFrame.h"
 
+// Declare a frame array as an hash table
+WX_DECLARE_HASH_MAP( wxFrame *,      // type of the keys
+                     wxFrame *,    // type of the values
+                     wxPointerHash,     // hasher
+                     wxPointerEqual,   // key equality predicate
+                     FrameHash); // name of the class
+
 // Define a new application
 class MutApp : public wxApp
 {
  public:
     bool OnInit();
-	void OnAbout (wxCommandEvent& event);
+	void CmAbout (wxCommandEvent& event);
+	void CmFileNew (wxCommandEvent& event);
+	void CmFileOpen (wxCommandEvent& event);
+	void CmQuit (wxCommandEvent& event);
 	MutFrame* CreateMainFrame();
+	void RegisterFrame(wxFrame * f);
+	void UnregisterFrame (wxFrame * f);
  private:
     wxLocale m_locale;
 	DECLARE_EVENT_TABLE()
+	
+	FrameHash frames;
+	bool quitting;
 };
 
 DECLARE_APP(MutApp)
