fa6ac318d6666de00a83c7d6feff8401496b7667
Tobias Schlememr <keinstein@users.berlios.de>
2007-12-17 12:42:20 +0000

    Use #defined Interface types for compilation in WX Unicode mode

--- mu32/DevGIS.h
+++ mu32/DevGIS.h
@@ -10,26 +10,28 @@
 #include "GIS_Head.h"
 //#include "Interpre.h"
 
-extern char *CurrentId;
-extern char *CurrentSep;
+extern mutString CurrentId;
+extern mutString CurrentSep;
 
 // OutGis -----------------------------------------------------------
 
 class OutGis : public OutDevice
 {
+  protected:
+		mutString Name;
   public:
-  	 GisWriteHead *Head;
-	   OutGis(char *Id)
-	   : OutDevice()
-	   {
-	    	Head = new GisWriteHead(0, Id);
-	   }
-	   ~OutGis()
-	   {
-		    delete Head;
-	   }
-	   virtual bool Open() { return true; }
-	   virtual void Close()
+		GisWriteHead *Head;
+
+		OutGis(mutString Id) : OutDevice(), Name(Id)
+		{
+			Head = new GisWriteHead(0, Id);
+		}	
+		~OutGis()
+		{
+			delete Head;
+		}
+		virtual bool Open() { return true; }
+		virtual void Close()
 	   {
 	    	CloseAllSubs(Head);
 	   };
@@ -37,16 +39,16 @@ class OutGis : public OutDevice
 	   virtual void NoteOff(int box, int taste, int velo, Route *r, int channel){};
 	   virtual void NotesCorrect(int box){};
  	   virtual void Sustain(char on, int channel) {};
-		 virtual int  GetChannel(int taste) { return -1; }
+		virtual int  GetChannel(int taste) { return -1; }
 	   virtual void Gis(GisToken *token, char turn);
  	   virtual void MidiOut(DWORD data, char n) {};
-  	 virtual void MidiOut(BYTE *p, char n) {};
+		virtual void MidiOut(BYTE *p, char n) {};
 	   virtual void AddTime(frac time)
 	   {
 	    	Head->AddTime(time);
 	   }
      virtual void Quite(Route *r) {};
-	   virtual char* GetName() { return "GisDevice In Test"; }
+	   virtual wxString & GetName() { return Name; }
 	   virtual DevType GetType() const { return DTGis; }
 };
 
@@ -55,32 +57,30 @@ class OutGis : public OutDevice
 class InGis : public InDevice
 {
   protected:
-  	 char Id[30];
-     char Name[180];
+  	 wxString Id;
+     wxString Name;
   	 GisToken *Data;
   	 GisReadArtHead *Head;
   public:
-  	InGis(char *name, char *id)
-	  : InDevice()
-	  {
-	    strcpy(Name, name);
-	    strcpy(Id, id);
-	    Head = 0;
-      Data = 0;
-	  }
-	  virtual bool Open();
-    virtual void Close();
-	  virtual void Stop();
-	  virtual void Play();
-	  virtual void Pause();
+  	InGis(wxString name, wxString id)
+	  : InDevice(),Name(name),Id(id)
+	{
+		Head = 0;
+		Data = 0;
+	}
+	virtual bool Open();
+	virtual void Close();
+	virtual void Stop();
+	virtual void Play();
+	virtual void Pause();
     void Proceed(GisReadArtHead *h, char turn, Route *route);
     void ProceedRoute(GisReadArtHead *h, char turn);
     virtual long ReadOn(long delta);
-  /*	 {
+  /*		{
  	return GisReadArtHeadOn(&Head, time, Proceed);
 	 }*/
-	   virtual char* GetName() { return Id; }
-	   virtual DevType GetType() const { return DTGis; }
+	virtual wxString &GetName() { return Id; }
+	virtual DevType GetType() const { return DTGis; }
     void IncDelta();
   private:
     long actDelta;  // in ticks
