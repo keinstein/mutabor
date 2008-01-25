4b26973c45bebf676ca96c940bc9c01ca5c9d2db
Tobias Schlememr <keinstein@users.berlios.de>
2008-01-25 09:37:11 +0000

    Enable CM_ACTIVATE, CM_FILENEW and CM_FILEOPEN in event table Inherit wxFrame Use AUIManager Let Application handle CM_FILENEW/CM_FILEOPEN if we have already a client New titles for Subframes Some Debug output use wxConfig
    MutFrame::CloseAll,
    MutFrame::OnSize,
    MutFrame::OnEraseBackground,
    MutFrame::OnActivate:        New Functions.

--- muwx/MutFrame.h
+++ muwx/MutFrame.h
@@ -1,11 +1,27 @@
 /////////////////////////////////////////////////////////////////////////////
+// $Id: MutFrame.h,v 1.8 2008/01/25 09:37:11 keinstein Exp $
 // Name:        MutFrame.h
 // Purpose:     Mutabor Frame
-// Author:      R. Krauﬂe
-// Modified by:
+// Author:      R. Krauße
+// Modified by: T. Schlemmer <keinstein@users.berlios.de>
 // Created:     12.08.05
-// Copyright:   (c) R. Krauﬂe
-// Licence:     wxWindows license
+// Copyright:   (c) R. Krauße, T. Schlemmer
+// Licence:     GPL
+// $Log: MutFrame.h,v $
+// Revision 1.8  2008/01/25 09:37:11  keinstein
+// Enable CM_ACTIVATE, CM_FILENEW and CM_FILEOPEN in event table
+// Inherit wxFrame
+// Use AUIManager
+// Let Application handle CM_FILENEW/CM_FILEOPEN if we have already a client
+// New titles for Subframes
+// Some Debug output
+// use wxConfig
+//
+// MutFrame::CloseAll,
+// MutFrame::OnSize,
+// MutFrame::OnEraseBackground,
+// MutFrame::OnActivate:        New Functions.
+//
 /////////////////////////////////////////////////////////////////////////////
 
 #ifndef MUTFRAME_H
@@ -29,24 +45,24 @@ public:
     void InitToolBar(wxToolBar* toolBar);
 
 //    void OnSize(wxSizeEvent& event);
-	void PassEventToEditor(wxCommandEvent &event);
+    void PassEventToEditor(wxCommandEvent &event);
     void EventPassOn(wxCommandEvent& event);
     void OnNewWindow(wxCommandEvent& event);
     void OnQuit(wxCommandEvent& event);
     void OnClose(wxCloseEvent& event);
-
-	void CmFileNew(wxCommandEvent& WXUNUSED(event));
-	void CmFileOpen(wxCommandEvent& WXUNUSED(event));
-
-	void OpenFile(wxString path);
-
-	void CmDoActivate(wxCommandEvent& event);
-	void CmStop(wxCommandEvent& WXUNUSED(event));
-	void CmPanic(wxCommandEvent& WXUNUSED(event));
+    
+    void CmFileNew(wxCommandEvent& WXUNUSED(event));
+    void CmFileOpen(wxCommandEvent& WXUNUSED(event));
+    
+    void OpenFile(wxString path);
+    
+    void CmDoActivate(wxCommandEvent& event);
+    void CmStop(wxCommandEvent& WXUNUSED(event));
+    void CmPanic(wxCommandEvent& WXUNUSED(event));
     void CeActivate(wxUpdateUIEvent& event);
     void CeStop(wxUpdateUIEvent& event);
 
-	void CmRoutes(wxCommandEvent& event);
+    void CmRoutes(wxCommandEvent& event);
 
     void CmToggleKey(wxCommandEvent& WXUNUSED(event));
     void CmToggleTS(wxCommandEvent& WXUNUSED(event));
@@ -60,45 +76,54 @@ public:
     void CeToggleCAW(wxUpdateUIEvent& event);
 
     // Recorder-Knˆpfe
-	void CmInDevStop(wxCommandEvent& WXUNUSED(event));
-	void CmInDevPlay(wxCommandEvent& WXUNUSED(event));
-	void CmInDevPause(wxCommandEvent& WXUNUSED(event));
-	void CeInDevStop(wxUpdateUIEvent& event);
-	void CeInDevPlay(wxUpdateUIEvent& event);
-	void CeInDevPause(wxUpdateUIEvent& event);
+    void CmInDevStop(wxCommandEvent& WXUNUSED(event));
+    void CmInDevPlay(wxCommandEvent& WXUNUSED(event));
+    void CmInDevPause(wxCommandEvent& WXUNUSED(event));
+    void CeInDevStop(wxUpdateUIEvent& event);
+    void CeInDevPlay(wxUpdateUIEvent& event);
+    void CeInDevPause(wxUpdateUIEvent& event);
 
-	// Idle
-	void UpdateUI(wxCommandEvent& WXUNUSED(event));
-	void OnIdle(wxIdleEvent& WXUNUSED(event));
-	void OnActivate(wxActivateEvent& event);
-	void OnEraseBackground(wxEraseEvent& event);
-	void OnSize(wxSizeEvent& event);
-	wxAuiDockArt* MutFrame::GetDockArt();
+    // Idle
+    void UpdateUI(wxCommandEvent& WXUNUSED(event));
+    void OnIdle(wxIdleEvent& WXUNUSED(event));
+    void OnActivate(wxActivateEvent& event);
+    void OnEraseBackground(wxEraseEvent& event);
+    void OnSize(wxSizeEvent& event);
+    wxAuiDockArt* MutFrame::GetDockArt();
+    
+    MutChild* NewFrame(WinKind winKind, 
+		       int box, 
+		       const wxString &frameName, 
+		       wxIcon icon, 
+		       const wxString &title = wxEmptyString);
+    void WindowSize(MutChild *win);
+    void SaveState();
+    void RestoreState();
 
-	MutChild* NewFrame(WinKind winKind, int box, const wxString &frameName, wxIcon icon, const wxString &title = wxEmptyString);
-	void WindowSize(MutChild *win);
-	void SaveState();
-	void RestoreState();
-
-	void LogicWinOpen(int box);
-	void ToggleTextBox(WinKind kind);
-	void TextBoxOpen(WinKind kind, int box);
-	void CmSelectBox();
-	void CmCloseChild();
+    void LogicWinOpen(int box);
+    void ToggleTextBox(WinKind kind);
+    void TextBoxOpen(WinKind kind, int box);
+    void CmSelectBox();
+    void CmCloseChild();
+    void CloseAll(WinKind kind);
 
     void SetStatus(int WXUNUSED(imgNr))
     {
       //StatusGadget->SelectImage(curStatusImg = imgNr, true);
     }
 	
-	wxRect DetermineFrameSize ();
-  private:
+    wxRect DetermineFrameSize ();
+ private:
     int curStatusImg;
 
-	wxAuiManager auimanager;
-
-	wxWindow *client;
-
+    wxAuiManager auimanager;
+    
+    wxWindow *keywindows[MAX_BOX];
+    wxWindow *tswindows[MAX_BOX];
+    wxWindow *actionwindows[MAX_BOX];
+    
+    wxWindow *client;
+    
     DECLARE_EVENT_TABLE()
 };
 
