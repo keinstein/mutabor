00dc6f73530c6542f34ba79350a9214c0873a1b4
Tobias Schlememr <keinstein@users.berlios.de>
2008-06-02 16:12:15 +0000

    Implement Help

--- muwx/MutConfDlg.h
+++ muwx/MutConfDlg.h
@@ -2,12 +2,15 @@
  ***********************************************************************
  * Mutabor Configuration Dialog.
  *
- * $Id: MutConfDlg.h,v 1.1 2008/01/25 10:04:30 keinstein Exp $
+ * $Id: MutConfDlg.h,v 1.2 2008/06/02 16:12:15 keinstein Exp $
  * \author Tobias Schlemmer <keinstein@users.berlios.de>
- * \date $Date: 2008/01/25 10:04:30 $
- * \version $Revision: 1.1 $
+ * \date $Date: 2008/06/02 16:12:15 $
+ * \version $Revision: 1.2 $
  *
  * $Log: MutConfDlg.h,v $
+ * Revision 1.2  2008/06/02 16:12:15  keinstein
+ * Implement Help
+ *
  * Revision 1.1  2008/01/25 10:04:30  keinstein
  * New file.
  *
@@ -27,11 +30,29 @@
 class MutConfigDialog : public ConfigDlg {
  public:
  MutConfigDialog(wxWindow * parent = NULL) : ConfigDlg(parent) {
+
     ToneSystem->SetValidator(wxGenericValidator((int *) &asTS));
     SaveEditor->SetValidator(wxGenericValidator(&::SaveEditor));
     ColorBars->SetValidator(wxGenericValidator(&UseColorBars));
+
+#if 0
+#ifndef __WXMSW__
+    wxSizer * buttonSizer = this->wxID_OK->GetParent()->GetSizer();
+      buttonSizer->Add(new wxContextHelpButton(this), 0, wxALIGN_CENTER|wxALL, 5);
+#endif
+#endif    
+
   }
+
   ~MutConfigDialog() {}
+
+  void CmHelp(wxCommandEvent& event) {
+    std::cerr << "MutConfigDialog::CmHelp" << std::endl;
+    HelpController->Display(_("The setup dialog"));
+    event.Skip();
+  }
+ private:
+  DECLARE_EVENT_TABLE()
 };
 
 #endif /* MUT_CONF_DLG_H */
