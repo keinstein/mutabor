9ab95d76346252fdbc6a63e7b3f0da869b89f336
Tobias Schlemmer <keinstein@users.berlios.de>
2008-06-30 08:31:21 +0000

    removed MutChildren. ~MutChild: outcourcing WinAttrs logic OnClose: removed WinAttrs code

--- muwx/MutChild.h
+++ muwx/MutChild.h
@@ -2,15 +2,20 @@
  ********************************************************************
  * Mutabor Mutabor Child Frame management.
  *
- * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/muwx/MutChild.h,v 1.5 2008/01/25 09:21:40 keinstein Exp $
+ * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/muwx/MutChild.h,v 1.6 2008/06/30 08:31:21 keinstein Exp $
  * Copyright:   (c) 2005,2006,2007 TU Dresden
  * \author Rüdiger Krauße <krausze@mail.berlios.de>
  * Tobias Schlemmer <keinstein@users.berlios.de>
- * \date $Date: 2008/01/25 09:21:40 $
- * \version $Revision: 1.5 $
+ * \date $Date: 2008/06/30 08:31:21 $
+ * \version $Revision: 1.6 $
  * \license wxWindows license
  * 
  * $Log: MutChild.h,v $
+ * Revision 1.6  2008/06/30 08:31:21  keinstein
+ * removed MutChildren.
+ * ~MutChild: outcourcing WinAttrs logic
+ * OnClose: removed WinAttrs code
+ *
  * Revision 1.5  2008/01/25 09:21:40  keinstein
  * MutChild: Inherit MutTextBox
  * Add EVT_AUI_PANE_CLOSE(MutChild::OnAuiClose) event handler
@@ -95,29 +100,72 @@ public:
   WinKind winKind;
   WinAttr *winAttr;
   MutChild (WinKind winkind, 
-		    WinAttr *winAttr, 
-		    wxWindow * parent= NULL, 
-		    wxWindowID id = -1,
-		    const wxPoint& pos = wxDefaultPosition,
+	    WinAttr *winAttr, 
+	    wxWindow * parent= NULL, 
+	    wxWindowID id = -1,
+	    const wxPoint& pos = wxDefaultPosition,
 	    const wxSize & size = wxDefaultSize);
   ~MutChild();
 
-//    void OnActivate(wxActivateEvent& event);
+  void OnActivate(wxActivateEvent& event);
 
 //    void OnRefresh(wxCommandEvent& event);
 //    void OnUpdateRefresh(wxUpdateUIEvent& event);
 //    void OnQuit(wxCommandEvent& event);
 //    void OnSize(wxSizeEvent& event);
 //    void OnMove(wxMoveEvent& event);
-    void OnClose(wxCloseEvent& event);
-    void OnAuiClose(wxAuiManagerEvent& event);
+
+  void deleteFromWinAttrs();
+
+  void OnClose(wxCloseEvent& event) {
+#ifdef DEBUG
+    std::cerr << "MutChild::OnClose" << std::endl;
+#endif
+    MutTextBox::OnClose(event);
+  }
+
+
+  void OnAuiClose(wxAuiManagerEvent& event) {
+#ifdef DEBUG
+    std::cerr << "MutChild::OnAuiClose" << std::endl;
+#endif
+    deleteFromWinAttrs();
+  }
+
+  // Override sizing for drawing the color
+
+  void GetClientSize(int * width, int * height) {
+    MutTextBox::GetClientSize(width,height);
+    if ((width -= 2) < 0) width = 0;
+    if ((height -=2) < 0) height =0; 
+  }
+
+  void SetClientSize(int width, int height) {
+    MutTextBox::SetClientSize(width+2, height+3);
+  }
+
+  void SetClientSize(const wxSize& size) {
+    wxSize s = size;
+    s.IncBy(2);
+    MutTextBox::SetClientSize(s);
+  }
+
+  void ClientToScreen(int * x, int * y ) {
+    MutTextBox::ClientToScreen(x,y);
+    x+=1; y+=1;
+  }
+  
+  wxPoint ClientToScreen(const wxPoint& pt) const{
+    return MutTextBox::ClientToScreen(pt)+wxPoint(1,1);
+  }
+
+    
 //	void MenuPassOn(wxCommandEvent& event);
 //	void MenuPassToParent(wxCommandEvent& event);
 
-    DECLARE_EVENT_TABLE()
+  DECLARE_EVENT_TABLE()
 };
 
-extern wxList MutChildren;
 extern int gs_nFrames;
 
 WinAttr* GetWinAttr(WinKind kind, int box = 0);
