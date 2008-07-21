3e8b65913f980cd86d9f51be9c35ee347830318d
Tobias Schlememr <keinstein@users.berlios.de>
2008-07-21 09:31:06 +0000

    some reformatting WinKind, WinAttr: moved to this file

--- muwx/MutTextBox.h
+++ muwx/MutTextBox.h
@@ -1,3 +1,4 @@
+// -*- C++ -*-
 /////////////////////////////////////////////////////////////////////////////
 // Name:        MutTextBox.h
 // Purpose:     Textbox for Lists
@@ -12,24 +13,54 @@
 #define MUTTEXTBOX_H
 
 #include <wx/listbox.h>
+#include "Global.h"
 // #include "MutChild.h"
 
+enum WinKind { WK_KEY = 0, WK_TS, WK_ACT, WK_LOGIC, WK_ROUTE, WK_EDIT, WK_NULL };
+
+inline WinKind operator++(WinKind & k)
+{ return k = WinKind(int(k) + 1); }
+
+inline WinKind operator++(WinKind & k,int)
+{ WinKind l=k; ++k; return l; }
+
+
+class WinAttr
+{
+ public:
+  wxWindow *Win;  // 0 = nicht offen
+  char Wanted;   // 0 = nicht wanted
+  //  int X, Y, W, H;     // W = 0 ... noch nicht benutzt, d.h. undefiniert
+  int Box;
+  WinAttr(char wanted = 0, int box = -1)
+    {
+      Wanted = wanted;
+      Box = box;
+      //      X = Y = H = W = 0;
+      Win = NULL;
+    }
+};
+
+WX_DECLARE_OBJARRAY(WinAttr, ArrayOfWinAttr);
+
+
 class MutTextBox : public wxListBox {
-  public:
-	MutTextBox(wxWindow* parent = NULL, 
-		   wxWindowID id=-1, 
-		   const wxPoint& pos = wxDefaultPosition, 
-		   const wxSize& size = wxDefaultSize);
-	void NewText(char *s, bool newTitle = false);
-	void NewText(wxString s, bool newTitle = false);
-	int ColorBox;
-	void OnClose(wxCloseEvent& event);
-  protected:
-/*	  void EvSize(uint sizeType, TSize &size);
-	  void EvChar(uint key, uint repeatCount, uint flags);
-	void EvSetFocus(HWND hWndLostFocus);
-	void EvRButtonDown(uint modKeys, TPoint& point);*/
-	  int Box() { return 0; } //PARENT_BOX; }
+protected:
+  WinKind winKind;
+  WinAttr *winAttr;
+public:
+  MutTextBox(  WinKind winKind,
+	       WinAttr *winAttr, 
+	       wxWindow* parent = NULL, 
+	     wxWindowID id=-1, 
+	     const wxPoint& pos = wxDefaultPosition, 
+	     const wxSize& size = wxDefaultSize);
+  void NewText(char *s, bool newTitle = false);
+  void NewText(wxString s, bool newTitle = false);
+  int ColorBox;
+  void OnClose(wxCloseEvent& event);
+protected:
+  int Box() { return winAttr->Box; }
 	DECLARE_EVENT_TABLE()
 };
 
