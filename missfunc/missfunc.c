2c51ccf1f8e8d98ef99e05e1b8e6e2700ee6c752
Tobias Schlememr <keinstein@users.berlios.de>
2008-11-05 13:07:08 +0000

    fix error building libmissfunc

--- missfunc/missfunc.c
+++ missfunc/missfunc.c
@@ -2,4 +2,7 @@
 #include "config.h"
 #endif
 
-const char * mutabor_missfunc_version = VERSION;
+static const char * version = VERSION;
+const char * mutabor_missfunc_version () {
+	return version;
+}
