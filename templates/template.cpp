e8ef5729aa5709c713926768530bdbec1e2a4702
Tobias Schlemmer <keinstein@users.berlios.de>
2011-08-06 09:16:22 +0000

    added “official” license descripiton

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,17 +1,35 @@
-/** \file 
+/** \file               -*- C++ -*-
  ********************************************************************
  * Description
  *
- * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/templates/template.cpp,v 1.3 2011/02/20 22:36:00 keinstein Exp $
- * Copyright:   (c) 2008 TU Dresden
+ * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/templates/template.cpp,v 1.4 2011/08/06 09:16:22 keinstein Exp $
+ * Copyright:   (c) 2011 TU Dresden
  * \author  Tobias Schlemmer <keinstein@users.berlios.de>
  * \date 
- * $Date: 2011/02/20 22:36:00 $
- * \version $Revision: 1.3 $
+ * $Date: 2011/08/06 09:16:22 $
+ * \version $Revision: 1.4 $
  * \license GPL
  *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
  * $Log: template.cpp,v $
- * Revision 1.3  2011/02/20 22:36:00  keinstein
+ * Revision 1.4  2011/08/06 09:16:22  keinstein
+ * added “official” license descripiton
+ *
+ * Revision 1.3  2011-02-20 22:36:00  keinstein
  * updated license information; some file headers have to be revised, though
  *
  * Revision 1.2  2010-11-21 13:15:51  keinstein
@@ -25,7 +43,7 @@
  * \addtogroup templates
  * \{
  ********************************************************************/
-// availlable groups: GUI, route, kernel, muwx, debug
+// availlable groups: GUI, route, kernel, muwx, debug, docview
 
 // ---------------------------------------------------------------------------
 // headers
