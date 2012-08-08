6df9779be184a142d882ed0144b2d3956b39cdcc
Tobias Schlemmer <keinstein@users.berlios.de>
2012-08-08 21:58:23 +0200

    Move RtMidi binaries into own librariy.

--- common-linklibs.mk
+++ common-linklibs.mk
@@ -1,4 +1,17 @@
-mutaborlibraries = $(top_builddir)/libmutaborrouting.a $(top_builddir)/libmutaborkernel.a $(top_builddir)/libmutabordebug.a
+mutaborlibraries = \
+	$(top_builddir)/libmutaborrouting.a \
+	$(top_builddir)/libmutaborkernel.a \
+	$(top_builddir)/libmutabordebug.a \
+	$(top_builddir)/librtmidi.a
 
-$(mutaborlibraries):
-	$(MAKE) $(AM_MAKEFLAGS) -C "$(top_builddir)" "`basename '$@'`"
+.PHONY: mkmutaborlibraries
+
+$(mutaborlibraries):mkmutaborlibraries
+
+mkmutaborlibraries:
+	for d in $(mutaborlibraries) ; \
+	do \
+		lib="`basename "$$d"`" ; \
+		echo "$$lib" ; \
+		$(MAKE) $(AM_MAKEFLAGS) -C "$(top_builddir)" "$$lib" || exit 1 ; \
+	done
