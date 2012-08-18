e3da280787399d113cbd4c40c96862b171da458f
Tobias Schlemmer <keinstein@users.berlios.de>
2012-08-18 23:28:18 +0200

    Clean up some comments.

--- src/wxGUI/Routing/DeviceShape.cpp
+++ src/wxGUI/Routing/DeviceShape.cpp
@@ -3,7 +3,6 @@
  ********************************************************************
  * Device shape base class for route window.
  *
- * $Header: /home/tobias/macbookbackup/Entwicklung/mutabor/cvs-backup/mutabor/mutabor/src/wxGUI/Routing/DeviceShape.cpp,v 1.8 2011/11/02 14:32:00 keinstein Exp $
  * \author Rüdiger Krauße <krausze@mail.berlios.de>,
  * Tobias Schlemmer <keinstein@users.berlios.de>
  * \date 2009/11/23
@@ -11,64 +10,6 @@
  * \version $Revision: 1.8 $
  * \license GPL
  *
- * $Log: DeviceShape.cpp,v $
- * Revision 1.8  2011/11/02 14:32:00  keinstein
- * fix some errors crashing Mutabor on Windows
- *
- * Revision 1.7  2011-10-04 05:38:44  keinstein
- * some configuration fixes
- *
- * Revision 1.6  2011-10-03 17:42:41  keinstein
- * Open the configuration dialog on key press in the route window
- * Accept entering nothing in the input/output device dialog
- *
- * Revision 1.5  2011-09-30 18:07:05  keinstein
- * * make compile on windows
- * * s/wxASSERT/mutASSERT/g to get assert handler completely removed
- * * add ax_boost_base for boost detection
- *
- * Revision 1.4  2011-09-27 20:13:24  keinstein
- * * Reworked route editing backend
- * * rewireing is done by RouteClass/GUIRoute now
- * * other classes forward most requests to this pair
- * * many bugfixes
- * * Version change: We are reaching beta phase now
- *
- * Revision 1.3  2011-02-20 22:35:58  keinstein
- * updated license information; some file headers have to be revised, though
- *
- * Revision 1.2  2010-11-21 13:15:48  keinstein
- * merged experimental_tobias
- *
- * Revision 1.1.2.3  2010-03-30 08:40:15  keinstein
- * added rudimentary command line support
- * changed debug system to allow selection of messages via command line
- * further enhancements to the route dialogs
- *
- * Revision 1.1.2.2  2010/02/15 12:08:21  keinstein
- * intermediate update for backup progress
- *
- * Revision 1.1.2.1  2009/11/30 12:08:57  keinstein
- * split MutRouteShapes into several files into directory Routing
- *
- * MutRouteShapes.cpp,v $
- * Revision 1.1.2.3  2009/11/03 15:07:07  keinstein
- * Compile on Ubuntu 9.10
- * remove several interface and implementation pragmas
- * fixed some file names for case insensitive filesystems
- *
- * Revision 1.1.2.2  2009/11/03 12:39:31  keinstein
- * input device dialog: Allow to edit input devices
- * fix a bug on Mac OS X, that prevented Mutabor from starting if no MIDI device is availlable
- *
- * Revision 1.1.2.1  2009/08/10 11:23:12  keinstein
- * merged from wrong tree
- *
- * Revision 1.2  2009/08/10 11:15:47  keinstein
- * some steps towards new route window
- *
- * Revision 1.1  2008/11/03 15:31:54  keinstein
- * Added to the system
  *
  *
  ********************************************************************
