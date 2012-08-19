c64cdfdad00d0eb8c6326050c737da3e66335b5b
Tobias Schlemmer <keinstein@users.berlios.de>
2012-08-19 18:10:16 +0200

    Add some missing lines to theading patch

--- src/kernel/routing/CommonFileDevice-inlines.h
+++ src/kernel/routing/CommonFileDevice-inlines.h
@@ -158,7 +158,9 @@ namespace mutabor {
 			return;
 		
 		if (timer && (timer != wxThread::This())) {
+			timer -> ClearFile();
 			timer -> Delete();
+			timer = NULL;
 		}
 
 		referenceTime = 0;
@@ -238,9 +240,12 @@ namespace mutabor {
 		}
 	}
 
-	inline wxThread::ExitCode CommonFileInputDevice::ThreadPlay()
+	inline wxThread::ExitCode
+	CommonFileInputDevice::ThreadPlay(FileTimer * thistimer)
 	{
+		/// \todo clean-up race conditions
 		mutASSERT(wxThread::This() == timer);
+		
 		mutASSERT(timer != NULL);
 		mutASSERT(Mode != DeviceCompileError );
 
@@ -250,21 +255,8 @@ namespace mutabor {
 		mutint64 delta; // in ms
 		wxThread::ExitCode e;
 		do {
-			mutASSERT(timer);
-			if (timer->TestDestroy()) {
-				switch (Mode) {
-				case DevicePause:
-				case DevicePlay: 
-					Mode = DeviceStop;
-				case DeviceStop:
-					e = 0;
-					break;
-				case DeviceCompileError:
-				default:
-		 			e = (void *)1;
-					mutASSERT(!"Thread should not have been run!");
-				}
-				return e;
+			if (thistimer->TestDestroy()) {
+				break;
 			}
 			nextEvent = PrepareNextEvent();
 			DEBUGLOG(timer,_T("Preparing next event is at %ld (isdelta=%d)"), nextEvent,IsDelta(nextEvent));
@@ -287,6 +279,8 @@ namespace mutabor {
 				}
 			}
 		} while (IsDelta(nextEvent));
+
+		
 		switch (Mode) {
 		case DevicePlay:
 		case DevicePause: 
@@ -299,9 +293,14 @@ namespace mutabor {
 			e = (void *)1;
 			break;
 		}
+
+		timer = NULL;
 		referenceTime = 0;
 		pauseTime = 0;
-		return NULL;
+
+	        thistimer->ClearFile(); 
+                        // this might delete us (smartptr).
+		return e;
 	}
 
 	
