9e7ce5f033c97bd7f4ead19a7b3c61b5467ac389
Tobias Schlemmer <keinstein@users.berlios.de>
2013-01-09 07:12:24 +0100

    Provide usleep function for windows.

--- src/kernel/routing/midi/tests/DevMidFTest.h
+++ src/kernel/routing/midi/tests/DevMidFTest.h
@@ -187,6 +187,18 @@ protected:
 	mutabor ::Route  route;
 	mutabor_box_type * box;
 	mutabor::ChannelData cd;
+#if __WXMSW__
+	void usleep(int waitTime) {
+		__int64 time1 = 0, time2 = 0, freq = 0;
+
+		QueryPerformanceCounter((LARGE_INTEGER *) &time1);
+		QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+		
+		do {
+			QueryPerformanceCounter((LARGE_INTEGER *) &time2);
+		} while((time2-time1) < waitTime);
+	}	
+#endif
 };
 
 #endif // precompiled
