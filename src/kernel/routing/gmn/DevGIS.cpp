f949f63e739d8d64558293da62051fc0ff9f3008
Tobias Schlemmer <keinstein@users.berlios.de>
2013-11-24 19:10:33 +0100

    Silence warning when GIS output device is loaded from a route configuration.

--- src/kernel/routing/gmn/DevGIS.cpp
+++ src/kernel/routing/gmn/DevGIS.cpp
@@ -193,7 +193,7 @@ namespace mutabor {
 		wxString oldpath = config.GetPath();
 #endif
 		config.toLeaf(_T("Guido File Output"));
-		STUBC;
+		/* currently we don't have any parameters, here */
 		mutUnused(route);
 		config.toParent();
 		mutASSERT(oldpath == config.GetPath());
@@ -224,7 +224,7 @@ namespace mutabor {
 		wxString oldpath = config.GetPath();
 #endif
 		config.toLeaf(_T("Guido File Output"));
-		STUBC;
+		/* nothing to be done here at the moment */
 		mutUnused(route);
 		config.toParent();
 		mutASSERT(oldpath == config.GetPath());
