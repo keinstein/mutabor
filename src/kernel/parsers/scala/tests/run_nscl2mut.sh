adf7d809ce6661ed6420cf51bb89f533d65925ae
Tobias Schlemmer <keinstein@users.sourcforge.net>
2016-07-24 22:43:02 +0200

    Make distcheck happy. Part 1: add/remove files to/from several dependencies

--- src/kernel/parsers/scala/tests/run_nscl2mut.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-env
-
-file="$1"
-
-$top_builddir/scl2mut $file ${file%.nscl}.mut && exit 1
-grep -q -f ${file%.nscl}.err ${file%.nscl}.log || exit 2
-exit 0
--- src/kernel/parsers/scala/tests/run_scl2mut.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-env
-
-file="$1"
-
-$top_builddir/scl2mut $file ${file%.scl}.mut 2>&1 >${file%.scl}.log || exit 1
-diff -u ${file}.mut ${file%.scl}.mut || exit 2
-exit 0
