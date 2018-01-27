6995a660243e24389534e835b8ffd076667a969f
Tobias Schlemmer <keinstein@users.sourceforge.net>
2018-01-27 21:15:12 +0100

    Configure the address sanitizer for mutabor.

--- src/wxGUI/generic/mutDebug.cpp
+++ src/wxGUI/generic/mutDebug.cpp
@@ -87,4 +87,129 @@ void ProcessDebugCommandLine(wxCmdLineParser&  parser)
 #endif
 
 
+
+#define EXTERNC extern "C" {
+#define EXTERNCEND }
+
+EXTERNC
+
+// Functions returning default options are declared weak in the tools' runtime
+// libraries. To make the linker pick the strong replacements for those
+// functions from this module, we explicitly force its inclusion by passing
+// -Wl,-u_sanitizer_options_link_helper
+void _sanitizer_options_link_helper() { }
+#define NO_SANITIZE							\
+	__attribute__((no_sanitize_address,				\
+		       no_sanitize_memory,				\
+		       no_sanitize_thread,				\
+		       no_sanitize_undefined,					\
+		       visibility("default"),				\
+		       used))
+
+// Default options for AddressSanitizer in various configurations:
+//   malloc_context_size=5 - limit the size of stack traces collected by ASan
+//     for each malloc/free by 5 frames. These stack traces tend to accumulate
+//     very fast in applications using JIT (v8 in Chrome's case), see
+//     https://code.google.com/p/address-sanitizer/issues/detail?id=177
+//   symbolize=1 - enable in-process symbolization.
+//   legacy_pthread_cond=1 - run in the libpthread 2.2.5 compatibility mode to
+//     work around libGL.so using the obsolete API, see
+//     http://crbug.com/341805. This may break if pthread_cond_t objects are
+//     accessed by both instrumented and non-instrumented binaries (e.g. if
+//     they reside in shared memory). This option is going to be deprecated in
+//     upstream AddressSanitizer and must not be used anywhere except the
+//     official builds.
+//   check_printf=1 - check the memory accesses to printf (and other formatted
+//     output routines) arguments.
+//   use_sigaltstack=1 - handle signals on an alternate signal stack. Useful
+//     for stack overflow detection.
+//   strip_path_prefix=/../../ - prefixes up to and including this
+//     substring will be stripped from source file paths in symbolized reports
+//   fast_unwind_on_fatal=1 - use the fast (frame-pointer-based) stack unwinder
+//     to print error reports. V8 doesn't generate debug info for the JIT code,
+//     so the slow unwinder may not work properly.
+//   detect_stack_use_after_return=1 - use fake stack to delay the reuse of
+//     stack allocations and detect stack-use-after-return errors.
+const char * mutASANoptions =
+    "legacy_pthread_cond=1 malloc_context_size=5 "
+    "symbolize=1 check_printf=1 use_sigaltstack=1 detect_leaks=1 "
+    "strip_path_prefix=/../../ fast_unwind_on_fatal=1 "
+    "detect_stack_use_after_return=1 detect_odr_violation=1 "
+    "allow_user_segv_handler=1 ";
+
+NO_SANITIZE const char *__asan_default_options() {
+  return mutASANoptions;
+}
+
+const char * mutASANsuppressions = "";
+NO_SANITIZE const char *__asan_default_suppressions() {
+	return mutASANsuppressions;
+}
+
+// Default options for ThreadSanitizer in various configurations:
+//   detect_deadlocks=1 - enable deadlock (lock inversion) detection.
+//   second_deadlock_stack=1 - more verbose deadlock reports.
+//   report_signal_unsafe=0 - do not report async-signal-unsafe functions
+//     called from signal handlers.
+//   report_thread_leaks=0 - do not report unjoined threads at the end of
+//     the program execution.
+//   print_suppressions=1 - print the list of matched suppressions.
+//   history_size=7 - make the history buffer proportional to 2^7 (the maximum
+//     value) to keep more stack traces.
+//   strip_path_prefix=/../../ - prefixes up to and including this
+//     substring will be stripped from source file paths in symbolized reports.
+const char * mutTSANoptions =
+    "detect_deadlocks=1 second_deadlock_stack=1 report_signal_unsafe=0 "
+    "report_thread_leaks=0 print_suppressions=1 history_size=7 "
+    "strict_memcmp=0 strip_path_prefix=../../ ";
+NO_SANITIZE const char *__tsan_default_options() {
+	return mutTSANoptions;
+}
+
+const char * mutTSANsuppressions = "";
+NO_SANITIZE const char *__tsan_default_suppressions() {
+	return mutTSANsuppressions;
+}
+
+// Default options for MemorySanitizer:
+//   intercept_memcmp=0 - do not detect uninitialized memory in memcmp() calls.
+//     Pending cleanup, see http://crbug.com/523428
+//   strip_path_prefix=/../../ - prefixes up to and including this
+//     substring will be stripped from source file paths in symbolized reports.
+const char * mutMSANoptions =
+    "intercept_memcmp=1 strip_path_prefix=../../ ";
+NO_SANITIZE const char *__msan_default_options() {
+	return mutMSANoptions;
+}
+
+// Default options for LeakSanitizer:
+//   print_suppressions=1 - print the list of matched suppressions.
+//   strip_path_prefix=/../../ - prefixes up to and including this
+//     substring will be stripped from source file paths in symbolized reports.
+const char * mutLSANoptions =
+    "print_suppressions=1 strip_path_prefix=../../ ";
+NO_SANITIZE const char *__lsan_default_options() {
+	return mutLSANoptions;
+}
+const char * mutLSANsuppressions =
+	// fontconfig is used by wxWidgets, so we don't
+	// want to debug it. This prevents TSAN from
+	// reporting the caches from fontconfig
+	"leak:libfontconfig\n"
+	"leak:libX11.so\n"
+	"leak:libglib-2.0.so\n";
+NO_SANITIZE const char *__lsan_default_suppressions() {
+	return mutLSANsuppressions;
+}
+
+// Default options for UndefinedBehaviorSanitizer:
+//   print_stacktrace=1 - print the stacktrace when UBSan reports an error.
+const char * mutUBoptions =
+    "print_stacktrace=1 strip_path_prefix=../../ ";
+NO_SANITIZE const char* __ubsan_default_options() {
+  return mutUBoptions;
+}
+
+EXTERNCEND
+
 ///\}
