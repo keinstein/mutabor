a7c58bfdb531167269f7107e92fd2b53302c933d
Tobias Schlemmer <keinstein@users.sourceforge.net>
2019-02-22 14:58:54 +0100

    fix run_mut.sh

--- src/tests/run_mut.sh
+++ src/tests/run_mut.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
+set -e
 . src/tests/run_variables.sh
 
 stemfile="${script%.mut}"
 file="${top_srcdir}/src/tests/runmut.mid"
 startkeys=""
 
-if test -f ${script}.variables
+if test -f ${srcscript%.mut}.variables
 then
-    . ${script}.variables
+    . ${srcscript%.mut}.variables
+fi
+if test -f ${script%.mut}.variables
+then
+    . ${script%.mut}.variables
 fi
 
-./midifilemutabor${EXEEXT} "$file" "${stemfile}-result.mid" "$srcscript" "$startkeys" 
+./midifilemutabor${EXEEXT} "$file" "${stemfile}-result.mid" "$srcscript" "$startkeys" || exit $?
 
 ./mf2txt${EXEEXT} "${stemfile}-result.mid" | \
     sed 's/\r$//' | \
